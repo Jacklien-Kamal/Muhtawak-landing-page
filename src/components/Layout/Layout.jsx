f0f2361a4475f61f8260ad091a7aa14a15513edd
jacklien <jacklienkamal442@gmail.com>
2026-06-03 09:14:38 +0300

    whatsapp icon

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -17,12 +17,12 @@ function Layout() {
                 <Tooltip title={isRTL ? 'تواصل معنا عبر واتساب' : 'Contact us on WhatsApp'} placement="top">
 
             <a href="https://api.whatsapp.com/send/?phone=966551123179&text&type=phone_number&app_absent=0" target="_blank" 
-     className={`fixed z-100 cursor-pointer w-10 h-10  md:w-12 md:h-12 flex justify-center items-center   text-6xl  bottom-10 ${isRTL ? 'right-4 md:right-16' : 'left-4 md:left-16'}  bg-gradient-to-r from-primary via-secondary to-third p-1 rounded-full`}>
+     className={`fixed z-100 cursor-pointer w-10 h-10  md:w-16 md:h-16 flex justify-center items-center   text-6xl  bottom-10 ${isRTL ? 'right-4 md:right-16' : 'left-4 md:left-16'}  bg-gradient-to-r from-primary via-secondary to-third p-1 rounded-full`}>
 
                 <span style={{ animationDuration: '2s' }}
                     className="absolute inset-0 rounded-full bg-gray-300 animate-ping opacity-50" />
 
-                    <FaWhatsapp className="text-2xl md:text-3xl text-white " />
+                    <FaWhatsapp className="text-2xl md:text-4xl text-white " />
             </a>
             </Tooltip>
             <Footer />
